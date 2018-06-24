108fe07f12b94b3e8cd0f3bd1aed10a41e6df52f
testtube123 <32375907+testtube123@users.noreply.github.com>
2018-06-24 00:40:02 -0400

    init.el

---
+++ init.el
@@ -0,0 +1,4 @@
+(global-linum-mode t )
+(set-face-foreground 'linum "#cd3333")
+(setq default-frame-alist '((background-color . "#8b814c")))
+(add-to-list 'default-frame-alist '(foreground-color ."#cfcfcf"))
