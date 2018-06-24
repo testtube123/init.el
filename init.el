75edfeaaf5d9252e54a55d93a0ec26d8ecd1c116
testtube123 <32375907+testtube123@users.noreply.github.com>
2018-06-24 14:00:29 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,4 +1,22 @@
 (global-linum-mode t )
-(set-face-foreground 'linum "#cd3333")
-(setq default-frame-alist '((background-color . "#8b814c")))
-(add-to-list 'default-frame-alist '(foreground-color ."#cfcfcf"))
+(set-face-foreground 'linum "#54ff9f")
+(setq default-frame-alist '((background-color . "#708090")))
+(add-to-list 'default-frame-alist '(foreground-color ."#00cd66"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "#006400" :width extra-expanded))))
+ '(font-lock-keyword-face ((t (:foreground "#000080"))))
+ '(link ((t (:foreground "#00bfff" :underline t))))
+ '(menu ((t (:background "color-232" :foreground "color-255" :inverse-video t))))
+ '(minibuffer-prompt ((t (:foreground "#7cfc00"))))
+ '(mode-line ((t (:background "color-255" :foreground "color-232" :box (:line-width -1 :style released-button)))))
+ '(widget-field ((t (:background "#00ced1" :foreground "#00ff7f")))))
