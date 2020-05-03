6425e97b2c29dca88a9134068c8400ce6d3e5b59
testtube123 <32375907+testtube123@users.noreply.github.com>
2020-05-03 01:18:54 -0400

    Add files via upload

---
+++ init.el
@@ -0,0 +1,28 @@
+(setq-default indent-tabs-mode nil)
+(setq c-default-style "bsd"
+ c-basic-offset 2)
+(global-display-line-numbers-mode)
+(load-theme #'rebecca t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   [("#303030" "#303030")
+    ("#5fafff" "#303030")
+    ("#af5fff" "#303030")
+    ("#afafd7" "#afafd7")
+    ("#5fd7af" "#5fd7af")
+    ("#87afd7" "#87afd7")
+    ("#87ffd7" "#87ffd7")
+    ("#afafff" "#afafff")])
+ '(custom-safe-themes
+   (quote
+    ("61591d084abe2d9beff49e304e2ae7f12101b67ad87f212c921556bd074d1f3a" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
