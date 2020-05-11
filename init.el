8db4ebdf90fc0af373fc78bf05bec817b2866f22
testtube123 <32375907+testtube123@users.noreply.github.com>
2020-05-11 11:33:08 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,3 +1,26 @@
+(;; Package Manager
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+;; init package.el
+(package-initialize)
+
+;; Define Function for auto-complete-c-headers and is called for C hooks
+(defun c:ac-c-header-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers))
+
+;; add this
+(add-hook 'c-mode-hook 'c:ac-c-header-init)
+(add-hook 'c++-mode-hook 'c:ac-c-header-init)
+;; yasnippet
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;; Auto Completion
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+;; Tabs,and indentation
 (setq-default indent-tabs-mode nil)
 (setq c-default-style "bsd"
  c-basic-offset 2)
@@ -19,7 +42,10 @@
     ("#afafff" "#afafff")])
  '(custom-safe-themes
    (quote
-    ("61591d084abe2d9beff49e304e2ae7f12101b67ad87f212c921556bd074d1f3a" default))))
+    ("61591d084abe2d9beff49e304e2ae7f12101b67ad87f212c921556bd074d1f3a" default)))
+ '(package-selected-packages
+   (quote
+    (auto-complete-c-headers yasnippet auto-complete ac-c-headers))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
